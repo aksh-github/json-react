574bfbfb122f0730b5ac3f96dedac5a01b532cb6
Akshay B <beatle02@gmail.com>
2022-06-21 22:50:24 +0530

    Stable changes App compo made generic

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import { inject, xml2json } from "./util";
 import { UIBuilder } from "./UIBuilder";
 import { GlobalProvider } from "./GlobalContext";
 
+const routeData = {
+  "/": {
+    dataKey: "intro",
+    xmlKey: "chat-intro.xml",
+  },
+  "/room": {
+    dataKey: "main",
+    xmlKey: "chat-main.xml",
+  },
+};
+
 const msgs = [
   {
     message: "hi",
@@ -55,21 +66,31 @@ const arr = ["akshay", "bapaye"];
 const Common = () => {};
 
 function App() {
+  const path = "/"; //this will be route
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState(msgs);
+  const [user, setUser] = useState("");
+  const [room, setRoom] = useState("");
 
   const _data = {
-    message: message,
-    messages: messages,
-    sendBtnFlag: message || message.length < 1,
+    intro: {
+      user: user,
+      room,
+      chatBtnFlag: user.length < 4 || room.length < 4,
+    },
+    main: {
+      message: message,
+      messages: messages,
+      sendBtnFlag: message || message.length < 1,
+    },
   };
   const [Ui, setUi] = useState(null);
 
   // console.log(Ui);
-  console.log("https://jasonformat.com/wtf-is-jsx/");
+  // console.log("https://jasonformat.com/wtf-is-jsx/");
 
   useEffect(() => {
-    fetch("chat-main.xml")
+    fetch(routeData[path].xmlKey)
       .then((res) => res.text())
       .then((res) => {
         // console.log(res);
@@ -82,7 +103,7 @@ function App() {
         //   doc.querySelectorAll("Import")[0].attributes
         // );
         const jsonSchema = xml2json(doc, {
-          ignoreTags: ["header2", "footer2"],
+          ignoreTags: [],
         });
         console.log(JSON.stringify(jsonSchema));
         setUi(jsonSchema);
@@ -115,93 +136,29 @@ function App() {
         // console.log("change in username");
         setMessage(e.target.value);
         break;
+      case "txtuser":
+        // console.log("change in username");
+        setUser(e.target.value);
+        break;
+      case "txtroom":
+        // console.log("change in roomname");
+        setRoom(e.target.value);
+        break;
     }
   };
 
   return (
     <div className="App2">
       <GlobalProvider
-        state={{ data: _data, fns: { buttonClicked, textChanged } }}
+        state={{
+          data: _data[routeData[path].dataKey],
+          fns: { buttonClicked, textChanged },
+        }}
       >
         {Ui ? <UIBuilder Ui={Ui} /> : "Loading..."}
       </GlobalProvider>
     </div>
   );
 }
-
-// function App() {
-//   const [user, setUser] = useState("");
-//   const [room, setRoom] = useState("");
-
-//   const _data = {
-//     user: user,
-//     room,
-//     chatBtnFlag: user.length < 4 || room.length < 4,
-//   };
-//   const [Ui, setUi] = useState(null);
-
-//   // console.log(Ui);
-//   console.log("https://jasonformat.com/wtf-is-jsx/");
-
-//   useEffect(() => {
-//     fetch("chat-intro.xml")
-//       .then((res) => res.text())
-//       .then((res) => {
-//         // console.log(res);
-//         const parser = new DOMParser();
-//         const doc = parser.parseFromString(res, "application/xml");
-//         // console.log(doc);
-//         // console.log(
-//         //   document.evaluate("/template", doc, null, XPathResult.ANY_TYPE, null),
-
-//         //   doc.querySelectorAll("Import")[0].attributes
-//         // );
-//         const jsonSchema = xml2json(doc, { ignoreTags: ["footer2"] });
-//         console.log(JSON.stringify(jsonSchema));
-//         setUi(jsonSchema);
-//       });
-//   }, []);
-
-//   const buttonClicked = (e) => {
-//     // console.log(e);
-
-//     switch (e.target?.name) {
-//       case "btnGo":
-//         if (user.length < 4 || room.length < 4) {
-//           console.log("> 4 reqd");
-//           return;
-//         }
-
-//         console.log("processed");
-
-//         break;
-//     }
-//   };
-
-//   const textChanged = (e) => {
-//     // console.log(e);
-
-//     switch (e.target?.name) {
-//       case "txtuser":
-//         // console.log("change in username");
-//         setUser(e.target.value);
-//         break;
-//       case "txtroom":
-//         // console.log("change in roomname");
-//         setRoom(e.target.value);
-//         break;
-//     }
-//   };
-
-//   return (
-//     <div className="App2">
-//       <GlobalProvider
-//         state={{ data: _data, fns: { buttonClicked, textChanged } }}
-//       >
-//         {Ui ? <UIBuilder Ui={Ui} /> : "Loading..."}
-//       </GlobalProvider>
-//     </div>
-//   );
-// }
 
 export default App;
